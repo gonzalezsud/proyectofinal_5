47d53bdf8b4b3a549702a23d8b7a2446a63d2c0c
Noe Gonzalez <gonzalezsud@yahoo.com>
2023-07-18 12:13:23 -0600

    1213

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="flex flex-col gap-6 text-xl">
                     <button class="btn-option w-24"><i class="fa-solid fa-list"></i></button>
-                    <button class="btn-option w-24"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+                    <button class="btn-option w-24" onclick="showHistory()"><i class="fa-solid fa-arrow-rotate-left"></i></button>
                     <button class="btn-option w-24"><i class="fa-solid fa-chart-simple"></i></button>
                 </div>
                 <div class="text-2xl">
@@ -50,9 +50,13 @@
                                 @foreach ($category->items as $item)
                                     <div class="bg-gray-200 border border-gray-400 rounded-lg p-4 flex items-center justify-center cursor-pointer"
                                          data-item-id="{{ $item->id }}"
-                                         onclick="showItemDetails(this)">
-                                        <span class="text-gray-600 mx-2">{{ $item->name }}</span>
-                                        <i class="fa-solid fa-plus"></i>
+                                         data-item-image="{{ $item->image }}"
+                                         onclick="showItemDetails(this)"
+                                         style="display: flex; align-items: center;">
+                                        <div>
+                                            <span class="text-gray-600 mx-2">{{ $item->name }}</span>
+                                            <i class="fa-solid fa-plus"></i>
+                                        </div>
                                     </div>
                                 @endforeach
                             </div>
@@ -62,15 +66,14 @@
             </table>
         </div>
         <div class="w-1/7 bg-gray-300 flex-grow">
-            <div class="my-6 mx-4" id="item-details">
-                <div class="bg-gray-200 border border-gray-400 rounded-lg p-4 flex items-center justify-center cursor-pointer"
-                data-item-id="{{ $item->id }}"
-                onclick="loadCreateItemForm('{{ route('items.create') }}')">
-               <span class="text-gray-600 mx-2">{{ $item->name }}</span>
-               <i class="fa-solid fa-plus"></i>
-           </div> <!-- Aquí se mostrará el contenido de la vista create.blade.php al hacer clic en un item -->
+            <div class="my-6 mx-4 flex-shrink-0" id="item-details">
+                @include('items.create')
+            </div>
+            <div class="my-6 mx-4 flex-shrink-0 hidden" id="history-container">
+                <!-- Aquí se mostrará el historial de items vistos -->
             </div>
         </div>
+    </div>
     <script>
         const botones = document.querySelectorAll(".btn-option");
         console.log(botones);
@@ -83,15 +86,21 @@
         const selectedItemsContainer = document.getElementById('selected-items');
         const itemDetailsContainer = document.getElementById('item-details');
         const backButton = document.querySelector('.p-4 button.text-gray-600');
+        const historyContainer = document.getElementById('history-container');
+        const historyItems = [];
 
         async function showItemDetails(itemElement) {
             const itemId = itemElement.getAttribute('data-item-id');
+            const itemImage = itemElement.getAttribute('data-item-image');
             const itemDetails = await getItemDetails(itemId);
+
+            historyItems.push(itemDetails); // Agregar item al historial
+            renderHistory(); // Actualizar el historial
 
             itemDetailsContainer.innerHTML = `
                 <div class="p-4">
                     <button class="text-gray-600" onclick="hideItemDetails()"><i class="fa-solid fa-arrow-left"></i> Back</button>
-                    <img src="{{ asset('storage/') }}/${itemDetails.image}" alt="${itemDetails.name}" class="w-16 h-16">
+                    <img src="${itemImage}" alt="${itemDetails.name}" class="w-16 h-16">
                     <p class="text-gray-600">${itemDetails.category_name}</p>
                     <p>${itemDetails.note}</p>
                 </div>
@@ -109,17 +118,40 @@
         }
 
         function loadCreateItemForm(url) {
-        // Realiza una petición AJAX para obtener el contenido de la vista create.blade.php
-        fetch(url)
-            .then(response => response.text())
-            .then(html => {
-                const itemDetailsContainer = document.getElementById('item-details');
-                itemDetailsContainer.innerHTML = html;
-            })
-            .catch(error => {
-                console.error('Error al cargar el formulario de creación de item:', error);
+            fetch(url)
+                .then(response => response.text())
+                .then(html => {
+                    const itemDetailsContainer = document.getElementById('item-details');
+                    itemDetailsContainer.innerHTML = html;
+                })
+                .catch(error => {
+                    console.error('Error al cargar el formulario de creación de item:', error);
+                });
+        }
+
+        function showHistory() {
+            historyContainer.classList.toggle('hidden');
+        }
+
+        function renderHistory() {
+            historyContainer.innerHTML = '';
+
+            if (historyItems.length === 0) {
+                historyContainer.innerHTML = '<p>No history available</p>';
+                return;
+            }
+
+            const historyList = document.createElement('ul');
+            historyList.classList.add('p-4');
+
+            historyItems.forEach(item => {
+                const listItem = document.createElement('li');
+                listItem.innerText = item.name;
+                historyList.appendChild(listItem);
             });
-    }
+
+            historyContainer.appendChild(historyList);
+        }
     </script>
 </body>
 </html>
